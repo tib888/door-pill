071de8e1f226ad7e9230a99e8fe26be9d98ec24a
Tibor Prokai <tibor@kotem.com>
2020-04-16 16:08:29 +0200

    updated to compile with the latest rtfm

--- src/main.rs
+++ src/main.rs
@@ -38,53 +38,47 @@
 //! C14, C15 used on the bluepill board for 32768Hz xtal
 //!
 
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 //#![deny(warnings)]
 #![no_main]
 #![no_std]
 
-extern crate cortex_m;
-extern crate panic_halt;
-#[macro_use]
-extern crate cortex_m_semihosting as sh;
-extern crate embedded_hal;
-extern crate ir;
-extern crate nb;
-extern crate onewire;
-extern crate room_pill;
-extern crate rtfm;
-extern crate stm32f103xx_hal as hal;
-
-use crate::hal::{
-    can::*, delay::Delay, gpio::*, prelude::*, rtc, stm32f103xx, watchdog::IndependentWatchdog,
+use stm32f1xx_hal::{
+    can::*, 
+    delay::Delay, 
+    gpio::{ExtiPin, *}, 
+    prelude::*, 
+    rtc, 
+    watchdog::IndependentWatchdog
 };
 use embedded_hal::{
-    digital::InputPin,
+    digital::v2::InputPin,
     watchdog::{Watchdog, WatchdogEnable},
 };
 use heapless::{
     consts::*,
+    i,
     spsc::{Consumer, Producer, Queue},
 };
-use ir::NecReceiver;
 use onewire::*;
 use room_pill::{
+    ir,
+    ir::NecReceiver,
     ir_remote::*,
     rgb::{Colors, RgbLed},
 };
-use rtfm::{app, Duration, Instant, U32Ext};
-// use stm32f103xx::Interrupt;
-// use sh::hio;
-// use core::fmt::Write;
+use rtfm::{
+    app,
+    cyccnt::{Duration, Instant, U32Ext},
+};
 
-#[derive(Copy, Clone)]
-pub struct MyInstant(rtfm::Instant);
 
-impl ir::Instant for MyInstant {
-    fn elapsed_us_till(&self, now: &Self) -> u32 {
-        0 //TODO now.0.duration_since(self.0).cycles() / 72;
-    }
-}
+#[cfg(not(feature = "itm-debug"))]
+use panic_halt as _;
+#[cfg(feature = "itm-debug")]
+use panic_itm as _;
+
+const MHZ: u32 = 72;
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum OnOff {
@@ -114,10 +108,9 @@ where
     }
 
     pub fn update_state(&mut self, now: Instant) -> OnOff {
-        if self.pin.is_high() {
+        if let Ok(true) = self.pin.is_high() {
             self.rised_at = Some(now);
-        };
-
+        }
         if let Some(rised) = self.rised_at {
             if now.duration_since(rised) <= self.ac_period {
                 return OnOff::On;
@@ -127,24 +120,22 @@ where
     }
 }
 
-#[app(device = stm32f103xx)]
+#[app(device = stm32f1xx_hal::pac, monotonic = rtfm::cyccnt::CYCCNT, peripherals = true)]
 const APP: () = {
-    // Late resources
-    static mut IR_FIFO_P: Producer<'static, ir::NecContent, U4> = ();
-    static mut IR_FIFO_C: Consumer<'static, ir::NecContent, U4> = ();
-    static mut WATCHDOG: IndependentWatchdog = ();
-    static mut SWITCH_A: AcSwitch2<hal::gpio::gpioa::PA2<hal::gpio::Input<hal::gpio::PullUp>>> = ();
-    static mut SWITCH_B: AcSwitch2<hal::gpio::gpioa::PA3<hal::gpio::Input<hal::gpio::PullUp>>> = ();
-    static mut SWITCH_C: AcSwitch2<hal::gpio::gpioa::PA1<hal::gpio::Input<hal::gpio::PullUp>>> = ();
-    static mut SWITCH_D: AcSwitch2<hal::gpio::gpioa::PA0<hal::gpio::Input<hal::gpio::PullUp>>> = ();
-    static mut IR_RECEIVER: hal::gpio::gpioa::PA15<hal::gpio::Input<hal::gpio::PullUp>> = ();
-    static mut NEC_RECEIVER: ir::IrReceiver<MyInstant> = ();
+    struct Resources {
+        ir_producer: Producer<'static, ir::NecContent, U4>,
+        ir_consumer: Consumer<'static, ir::NecContent, U4>,
+        WATCHDOG: IndependentWatchdog,
+        SWITCH_A: AcSwitch2<stm32f1xx_hal::gpio::gpioa::PA2<stm32f1xx_hal::gpio::Input<stm32f1xx_hal::gpio::PullUp>>>,
+        SWITCH_B: AcSwitch2<stm32f1xx_hal::gpio::gpioa::PA3<stm32f1xx_hal::gpio::Input<stm32f1xx_hal::gpio::PullUp>>>,
+        SWITCH_C: AcSwitch2<stm32f1xx_hal::gpio::gpioa::PA1<stm32f1xx_hal::gpio::Input<stm32f1xx_hal::gpio::PullUp>>>,
+        SWITCH_D: AcSwitch2<stm32f1xx_hal::gpio::gpioa::PA0<stm32f1xx_hal::gpio::Input<stm32f1xx_hal::gpio::PullUp>>>,
+        IR_RECEIVER: stm32f1xx_hal::gpio::gpioa::PA15<stm32f1xx_hal::gpio::Input<stm32f1xx_hal::gpio::PullUp>>,
+        NEC_RECEIVER: ir::IrReceiver<Instant>,
+    }
 
     #[init]
-    // #[init(schedule = [foo], spawn = [foo])]
-    fn init() {
-        // let now = Instant::now();
-
+    fn init(cx: init::Context) -> init::LateResources {
         // all interrupts are disabled here
         // let _: Instant = start;
         // let _: rtfm::Peripherals = core;
@@ -152,8 +143,10 @@ const APP: () = {
         // let _: init::Schedule = schedule;
         // let _: init::Spawn = spawn;
 
+        let device = cx.device;
+
         let mut watchdog = IndependentWatchdog::new(device.IWDG);
-        watchdog.start(2_000_000u32.us());
+        watchdog.start(2_000u32.ms());
 
         let mut flash = device.FLASH.constrain();
 
@@ -164,38 +157,58 @@ const APP: () = {
         let clocks = rcc
             .cfgr
             .use_hse(8.mhz())
-            //.sysclk(72.mhz())
-            //.hclk(72.mhz())
-            //.pclk1(36.mhz())
-            //.pclk2(72.mhz())
-            //.adcclk(12.mhz())
+            .sysclk(MHZ.mhz())
+            .hclk(MHZ.mhz())
+            .pclk1(36.mhz())
+            .pclk2(MHZ.mhz())
+            .adcclk(12.mhz())
             .freeze(&mut flash.acr);
 
         watchdog.feed();
 
         // real time clock
-        let rtc = rtc::Rtc::new(device.RTC, &mut rcc.apb1, &mut device.PWR);
+        let mut pwr = device.PWR;
+        let mut backup_domain = rcc.bkp.constrain(device.BKP, &mut rcc.apb1, &mut pwr);
+        let _rtc = rtc::Rtc::rtc(device.RTC, &mut backup_domain);
         watchdog.feed();
-
-        let mut afio = device.AFIO.constrain(&mut rcc.apb2);
-        // Disables the JTAG to free up pb3, pb4 and pa15 for normal use
-        afio.mapr.disable_jtag();
 
         // Configure pins:
         let mut gpioa = device.GPIOA.split(&mut rcc.apb2);
+        let mut gpiob = device.GPIOB.split(&mut rcc.apb2);
+        let mut gpioc = device.GPIOC.split(&mut rcc.apb2);
+
+        let mut afio = device.AFIO.constrain(&mut rcc.apb2);
+        // Disables the JTAG to free up pb3, pb4 and pa15 for normal use
+        let (pa15, _pb3_itm_swo, pb4) = afio.mapr.disable_jtag(gpioa.pa15, gpiob.pb3, gpiob.pb4);
 
         // Switces on A0, A1, A2, A3 (pull down once in each main period if closed)
         let ac_period = (clocks.sysclk().0 / 50u32).cycles();
-        let mut switch_d = AcSwitch2::new(ac_period, gpioa.pa0.into_pull_up_input(&mut gpioa.crl));
-        let mut switch_c = AcSwitch2::new(ac_period, gpioa.pa1.into_pull_up_input(&mut gpioa.crl));
-        let mut switch_a = AcSwitch2::new(ac_period, gpioa.pa2.into_pull_up_input(&mut gpioa.crl));
-        let mut switch_b = AcSwitch2::new(ac_period, gpioa.pa3.into_pull_up_input(&mut gpioa.crl));
+
+        let mut pa0 = gpioa.pa0.into_pull_up_input(&mut gpioa.crl);
+        pa0.make_interrupt_source(&mut afio);
+        pa0.trigger_on_edge(&device.EXTI, Edge::RISING_FALLING);
+        let switch_d = AcSwitch2::new(ac_period, pa0);
+
+        let mut pa1 = gpioa.pa1.into_pull_up_input(&mut gpioa.crl);
+        pa1.make_interrupt_source(&mut afio);
+        pa1.trigger_on_edge(&device.EXTI, Edge::RISING_FALLING);
+        let switch_c = AcSwitch2::new(ac_period, pa1);
+
+        let mut pa2 = gpioa.pa2.into_pull_up_input(&mut gpioa.crl);
+        pa2.make_interrupt_source(&mut afio);
+        pa2.trigger_on_edge(&device.EXTI, Edge::RISING_FALLING);
+        let switch_a = AcSwitch2::new(ac_period, pa2);
+
+        let mut pa3 = gpioa.pa3.into_pull_up_input(&mut gpioa.crl);
+        pa3.make_interrupt_source(&mut afio);
+        pa3.trigger_on_edge(&device.EXTI, Edge::RISING_FALLING);
+        let switch_b = AcSwitch2::new(ac_period, pa3);
 
         // Motion alarm on A4 (pull down)
-        let motion_alarm = gpioa.pa4.into_pull_up_input(&mut gpioa.crl);
+        let _motion_alarm = gpioa.pa4.into_pull_up_input(&mut gpioa.crl);
 
         // Open alarm on A5 (pull down)
-        let open_alarm = gpioa.pa5.into_pull_up_input(&mut gpioa.crl);
+        let _open_alarm = gpioa.pa5.into_pull_up_input(&mut gpioa.crl);
 
         // A6, A7 not used, connected to the ground
         let _a6 = gpioa.pa6.into_pull_down_input(&mut gpioa.crl);
@@ -205,17 +218,17 @@ const APP: () = {
         let _piezzo = gpioa.pa8.into_open_drain_output(&mut gpioa.crh);
 
         // Solid state relay connected to A9 drives the lamp_b
-        let mut ssr_lamp_b = gpioa.pa9.into_push_pull_output(&mut gpioa.crh);
+        let _ssr_lamp_b = gpioa.pa9.into_push_pull_output(&mut gpioa.crh);
 
         // Solid state relay connected to A10 drives the lamp_a
-        let mut ssr_lamp_a = gpioa.pa10.into_push_pull_output(&mut gpioa.crh);
+        let _ssr_lamp_a = gpioa.pa10.into_push_pull_output(&mut gpioa.crh);
 
         // CAN (RX, TX) on A11, A12
         let canrx = gpioa.pa11.into_floating_input(&mut gpioa.crh);
         let cantx = gpioa.pa12.into_alternate_push_pull(&mut gpioa.crh);
         // USB is needed here because it can not be used at the same time as CAN since they share memory:
-        let mut can = Can::can1(
-            device.CAN,
+        let _can = Can::can1(
+            device.CAN1,
             (cantx, canrx),
             &mut afio.mapr,
             &mut rcc.apb1,
@@ -223,21 +236,22 @@ const APP: () = {
         );
 
         // Read the NEC IR remote commands on A15 GPIO as input with internal pullup
-        let ir_receiver = gpioa.pa15.into_pull_up_input(&mut gpioa.crh);
-
-        let mut gpiob = device.GPIOB.split(&mut rcc.apb2);
+        let ir_receiver = pa15.into_pull_up_input(&mut gpioa.crh);
 
         // Photoresistor on B0 (ADC8)
-        let photoresistor = gpiob.pb0.into_floating_input(&mut gpiob.crl);
+        let _photoresistor = gpiob.pb0.into_floating_input(&mut gpiob.crl);
 
         // B1 not connected
         let _b1 = gpiob.pb1.into_pull_down_input(&mut gpiob.crl);
 
         // B3 not used, connected to the ground
-        let _b3 = gpiob.pb3.into_pull_down_input(&mut gpiob.crl);
+        #[cfg(not(feature = "itm-debug"))]
+        let _b3 = _pb3_itm_swo.into_pull_down_input(&mut gpiob.crl);
+        #[cfg(feature = "itm-debug")]
+        let _b3 = _pb3_itm_swo.into_push_pull_output(&mut gpiob.crl);
 
         // DS18B20 1-wire temperature sensors connected to B4 GPIO
-        let onewire_io = gpiob.pb4.into_open_drain_output(&mut gpiob.crl);
+        let onewire_io = pb4.into_open_drain_output(&mut gpiob.crl);
 
         // B5 not used, connected to the ground
         let _b5 = gpiob.pb5.into_pull_down_input(&mut gpiob.crl);
@@ -249,7 +263,7 @@ const APP: () = {
         let _ssr_3 = gpiob.pb9.into_push_pull_output(&mut gpiob.crh);
 
         // PT8211 DAC (BCK, DIN, WS) on B10, B11, B12
-        let dac = room_pill::dac::Pt8211::new(
+        let _dac = room_pill::dac::Pt8211::new(
             gpiob.pb10.into_push_pull_output(&mut gpiob.crh), //use as SCL?
             gpiob.pb11.into_push_pull_output(&mut gpiob.crh), //use as SDA?
             gpiob.pb12.into_push_pull_output(&mut gpiob.crh), //word select (left / right^)
@@ -263,76 +277,47 @@ const APP: () = {
         );
         rgb.color(Colors::Black);
 
-        let mut gpioc = device.GPIOC.split(&mut rcc.apb2);
-
         // C13 on board LED^
         let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+        //led.set_high().unwrap();
 
         // C14, C15 used on the bluepill board for 32768Hz xtal
         watchdog.feed();
 
-        let cp = cortex_m::Peripherals::take().unwrap();
+        let mut delay = Delay::new(cx.core.SYST, clocks);
+        let mut _one_wire = OneWirePort::new(onewire_io, delay);
 
-        let mut delay = Delay::new(cp.SYST, clocks);
-        let mut one_wire = OneWirePort::new(onewire_io, delay);
-
-        let mut receiver = ir::IrReceiver::<MyInstant>::new();
-
-        // Schedule `bar` to run 4e6 cycles in the future
-        // schedule.foo(now + 4_000_000.cycles()).unwrap();
-
-        // NOTE: we use `Option` here to work around the lack of
-        // a stable `const` constructor
-        static mut Q: Option<Queue<ir::NecContent, U4>> = None;
-        Q = Some(Queue::new());
-        let (p, c) = Q.as_mut().unwrap().split();
-
+        let receiver = ir::IrReceiver::<Instant>::new();
+        
         //setup external interrupts EXTI0,1,2,3,4,EXTI9_5,EXTI15_10
-
-        // A0 = switch_d, A1 = switch_c, A2 = switch_a, A3 = switch_b,
-        // A4 = Motion,
-        // A5 = Open,
-        // A15 = ir_remote
-        // -> trigger on rising edge
-        device
-            .EXTI
-            .rtsr
-            .write(|w| w.bits(0b_0000_0000_0000_0000_1000_0000_0011_1111));
-
-        // A0 = switch_d, A1 = switch_c, A2 = switch_a, A3 = switch_b,
-        // A4 = Motion,
-        // A5 = Open,
-        // A15 = ir_remote
-        // -> trigger on rising edge
-        device
-            .EXTI
-            .ftsr
-            .write(|w| w.bits(0b_0000_0000_0000_0000_1000_0000_0011_1111));
-
-        //device.EXTI.imr // interrupt mask register - handled by rtfm when finished with init()?
-        //device.EXTI.emr // event mask - not needed
-        //device.EXTI.swier // software interrupt request - not needed
-        //device.EXTI.pr // pending request - handled by rtfm when exiting from irq?
-
-        // Initialization of late resources
-        WATCHDOG = watchdog;
-        IR_FIFO_P = p;
-        IR_FIFO_C = c;
-        SWITCH_A = switch_a;
-        SWITCH_B = switch_b;
-        SWITCH_C = switch_c;
-        SWITCH_D = switch_d;
-        NEC_RECEIVER = receiver;
-        IR_RECEIVER = ir_receiver;
+                     
+        static mut IR_QUEUE: Queue<ir::NecContent, U4> = Queue(i::Queue::new());
+        
+        unsafe {
+            let (ir_producer, ir_consumer) = IR_QUEUE.split();
+            
+            // Initialization of late resources
+            init::LateResources {
+                ir_producer: ir_producer,
+                ir_consumer: ir_consumer,
+                WATCHDOG: watchdog,            
+                SWITCH_A: switch_a,
+                SWITCH_B: switch_b,
+                SWITCH_C: switch_c,
+                SWITCH_D: switch_d,
+                NEC_RECEIVER: receiver,
+                IR_RECEIVER: ir_receiver,
+            }
+        }
     }
 
-    #[idle(resources = [WATCHDOG, IR_FIFO_C])]
-    fn idle() -> ! {
+    #[idle(resources = [WATCHDOG, ir_consumer])] 
+    fn idle(c: idle::Context) -> ! {
         // the interrupts are enabled here
         loop {
-            resources.WATCHDOG.feed();
+            c.resources.WATCHDOG.feed();
 
-            if let Some(ir_cmd) = resources.IR_FIFO_C.dequeue() {
+            if let Some(ir_cmd) = c.resources.ir_consumer.dequeue() {
                 match ir_cmd {
                     ir::NecContent::Repeat => {}
                     ir::NecContent::Data(data) => {
@@ -346,62 +331,71 @@ const APP: () = {
         }
     }
 
-    #[interrupt(
+    #[task(
+        binds = EXTI0,
         priority = 1,
         resources = [SWITCH_D],
     )]
-    fn EXTI0() {
-        let state = resources.SWITCH_D.update_state(start);
+    fn exti0(c: exti0::Context) {
+        let state = c.resources.SWITCH_D.update_state(Instant::now()); 
         //TODO on state change notify the main loop...
     }
 
-    #[interrupt(
+    #[task(
+        binds = EXTI1,
         priority = 1,
         resources = [SWITCH_C],
     )]
-    fn EXTI1() {
-        let state = resources.SWITCH_C.update_state(start);
+    fn exti1(c: exti1::Context) {
+        let state = c.resources.SWITCH_C.update_state(Instant::now());
         //TODO on state change notify the main loop...
     }
 
-    #[interrupt(
+    #[task(
+        binds = EXTI2,
         priority = 1,
         resources = [SWITCH_A],
     )]
-    fn EXTI2() {
-        let state = resources.SWITCH_A.update_state(start);
+    fn exti2(c: exti2::Context) {
+        let state = c.resources.SWITCH_A.update_state(Instant::now());
         //TODO on state change notify the main loop...
         //and toggle lamp A?
     }
 
-    #[interrupt(
+    #[task(
+        binds = EXTI3,
         priority = 1,
         resources = [SWITCH_B],
     )]
-    fn EXTI3() {
-        let state = resources.SWITCH_B.update_state(start);
+    fn exti3(c: exti3::Context) {
+        let state = c.resources.SWITCH_B.update_state(Instant::now());
         //TODO on state change notify the main loop...
         //and toggle lamp B?
     }
 
-    #[interrupt(
+    #[task(
+        binds = EXTI15_10,
         priority = 2,
-        resources = [IR_FIFO_P, NEC_RECEIVER, IR_RECEIVER],
+        resources = [NEC_RECEIVER, IR_RECEIVER, ir_producer],   
         // schedule = [foo],
         // spawn = [foo, bar],
     )]
-    fn EXTI15_10() {
+    fn exti15_10(c: exti15_10::Context) {
         // let _: Instant = start;
         // let _: resources::IR_FIFO_P = resources.IR_FIFO_P;
         // let _: EXTI0::Schedule = schedule;
         // let _: EXTI0::Spawn = spawn;
+        let now = Instant::now();
+
+        let ir_cmd = c.resources.NEC_RECEIVER.receive(
+            c.resources.IR_RECEIVER.is_low().unwrap(),
+            now,
+            |last| now.duration_since(last).as_cycles() / MHZ,
+        ); //in case of 72MHz, 72 clock = 1 microsecond
 
         // update the IR receiver statemachine:
-        if let Ok(ir_cmd) = resources
-            .NEC_RECEIVER
-            .receive(MyInstant(start), resources.IR_RECEIVER.is_low())
-        {
-            resources.IR_FIFO_P.enqueue(ir_cmd);
+        if let Ok(ir_cmd) = ir_cmd {
+            c.resources.ir_producer.enqueue(ir_cmd);
         }
     }
 
