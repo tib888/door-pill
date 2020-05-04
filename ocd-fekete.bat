9385a686c55af7a78c5649a23328f69dce79a883
Tibor Prokai <tibor@kotem.com>
2020-05-04 09:53:17 +0200

    debugger connection improved

---
+++ ocd-fekete.bat
@@ -0,0 +1,8 @@
+
+openocd -f "stlink-v2-fekete.cfg" -f "stm32f1x.cfg"
+
+rem run this first and leave the terminal open: 
+rem itmdump -f itm.fifo -s 0
+rem gdb port 5000
+rem telnet port 4444
+rem arm-none-eabi-gdb c:\Eclipse.portable\RustForSTM32\cortex-m-quickstart\target\thumbv7m-none-eabi\debug\libcortex_m_quickstart
